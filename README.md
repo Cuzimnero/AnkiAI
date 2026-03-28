e9b863ca59103eab6cf80789d0c35884aecda271
Cuzimnero <bergerarne1@gmx.de>
2026-03-28 16:37:43 +0100

    Improved Card rework

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
 # AnkiAI 🧠
 
-AnkiAI is a powerful tool designed to streamline your learning workflow by leveraging DeepSeek AI to generate flashcards for Anki.
+AnkiAI is a powerful tool designed to streamline your learning workflow by leveraging DeepSeek AI to generate flashcards
+for Anki.
 
 ---
 
 ## ⚠️ Notes
 
-* **AI Determinism:** Please be aware that Large Language Models (LLMs) are non-deterministic by nature. This means that for the same input, the results—including the number of cards, level of detail, and formatting—may vary between runs.
+* **AI Determinism:** Please be aware that Large Language Models (LLMs) are non-deterministic by nature. This means that
+  for the same input, the results—including the number of cards, level of detail, and formatting—may vary between runs.
 
-* **Local Execution (Ollama):** Running models locally ensures 100% privacy, but it involves significant hardware and performance trade-offs:
-    * **System Resources:** Generation is resource-intensive and relies heavily on your hardware. A minimum of 8 GB VRAM is recommended for a smooth experience. Running out of VRAM (Video RAM) will significantly degrade performance or can cause hallucination, the application to crash.
-    * **Performance:** Local inference takes significantly longer than cloud-based APIs. Expect a slower "cards-per-minute" rate depending on your system's power.
+* **Local Execution (Ollama):** Running models locally ensures 100% privacy, but it involves significant hardware and
+  performance trade-offs:
+    * **System Resources:** Generation is resource-intensive and relies heavily on your hardware. A minimum of 8 GB VRAM
+      is recommended for a smooth experience. Running out of VRAM (Video RAM) will significantly degrade performance or
+      can cause hallucination, the application to crash.
+    * **Performance:** Local inference takes significantly longer than cloud-based APIs. Expect a slower "
+      cards-per-minute" rate depending on your system's power.
 
 ---
 
 ## 🤖 Recommended Local Models
+
+You can use every Model which supports tool calls.
 For the best balance between speed and logic when generating Anki cards via Ollama, I recommend:
 
 * **Llama 3.1 (8B):** Currently the best all-rounder. Fast, reliable, and follows complex instructions well.
@@ -24,6 +32,7 @@ For the best balance between speed and logic when generating Anki cards via Olla
 ---
 
 ## ✨ Features
+
 * **DeepSeek Integration:** Uses DeepSeek's powerful LLM to extract and format knowledge into flashcards.
 * **Ollama Integration:** Support for local LLMs via Ollama ensures your data never leaves your machine.
 * **Direct Export:** Generates files ready for Anki import (no additional plugins like AnkiConnect required).
@@ -31,14 +40,41 @@ For the best balance between speed and logic when generating Anki cards via Olla
 
 ---
 
+## How it works
+
+```mermaid
+flowchart TD
+    A[PDF] -->|Extracting PDF Slide 1| B(LLM Gen-Agent Instance 1)
+    A[PDF] -->|Extracting PDF Slide 2 . . .| C(LLM Gen-Agent Instance 2 ..)
+    A[PDF] -->|Extracting PDF Slide N| D(LLM Gen-Agent Instance N)
+    B[LLM Gen-Agent Instance 1] -->|Generating Flash Cards| E(Result Collection)
+    C[LLM Gen-Agent Instance 2 ..] -->|Generating Flash Cards| E(Result Collection)
+    D[LLM Gen-Agent Instance N] -->|Generating Flash Cards| E(Result Collection)
+    E -->|Equal Parts| F(LLM Rework Agent 1)
+    E -->|Equal Parts| G(LLM Rework Agent 2 ...)
+    E -->|Equal Parts| H(LLM Rework Agent N)
+    F -->|Well cards| I(Reworked Cards)
+    G -->|Well cards| I(Reworked Cards)
+    H -->|Well cards| I(Reworked Cards)
+    F -->|Worse cards + Rework - Reason| J(Improver Agent)
+    G -->|Worse cards + Rework - Reason| J(Improver Agent)
+    H -->|Worse cards + Rework - Reason| J(Improver Agent)
+    J -->|Improved cards| I
+    I --> L(Embedding LLM)
+    L --> S(Cosine Similarity Matrix)
+    S -->|Deleting similar Cards| O(Flashcards)
+   ```
+
 ## 🚀 Getting Started
 
 ### Prerequisites
+
 * Python 3.10+
 * A DeepSeek API Key
 * Anki (for importing the generated cards)
 
 ### Installation
+
 1. **Clone the repository:**
    ```bash
    git clone [https://github.com/Cuzimnero/AnkiAI.git](https://github.com/Cuzimnero/AnkiAI.git)
--- src/ai/anki_gen.py
+++ src/ai/anki_gen.py
@@ -4,47 +4,76 @@ import math
 import os
 import pathlib
 from ftplib import print_line
-from pathlib import Path
+
 import customtkinter as ctk
-from . import embedding
 import ollama
 from dotenv import load_dotenv
-from httpx import options
 from openai import OpenAI
+
 from handler.pdf_handler import pdf_handler
+from . import embedding
 
 
 class AnkiGen:
-    def __init__(self, model_type:int, model:str):
-        self.model=model
-        self.type=model_type
+    def __init__(self, model_type: int, model: str):
+        self.model = model
+        self.type = model_type
         load_dotenv()
-        if self.type==1:
-            self.workers=30
-            self.rework_size=150
+        if self.type == 1:
+            self.workers = 30
+            self.rework_size = 50
         else:
-            self.workers=3
-            self.rework_size=30
+            self.workers = 3
+            self.rework_size = 30
 
-    def set_pdf_handler(self,path:pathlib.Path):
+    def set_pdf_handler(self, path: pathlib.Path):
         self.handler = pdf_handler(path)
 
-    def set_model(self,model:str):
+    def set_model(self, model: str):
         self.model = model
-        
 
-    def rework_part(self,cards: list[dict]):
-        system_prompt = f"""
-                You are an expert Anki content optimizer. 
-                Your task is to take a large, redundant list of flashcards and refine it into a high-quality, concise study deck.
-                
-                RULES:
-                1. DELETE: Remove all cards regarding organizational data, job offers, university-specific meta-info.
-                2. PRIORITIZE: Keep only core definitions, technical concepts, and ethical theories.
-                3. COMPLETENESS: Ensure that every technical term mentioned in the input cards is still represented in the final output.
-                4. Delete cards only containing one word as front.
-                Output MUST be a valid JSON object containing a list called 'cards' with 'front', 'back', and 'topic' fields.
-                """
+    def rework_part(self, cards: list[dict]):
+        system_prompt = r"""You are an expert Anki Content Optimizer. Your task is to filter, categorize, and refine a list of flashcards.
+
+        ### DECISION MATRIX:
+        You MUST return a valid JSON object with the following structure:
+        {
+          "keep": [
+            {"front": "...", "back": "...", "topic": "..."}
+          ],
+          "rework": [
+           {
+              "front": "...", 
+              "back": "...", 
+              "topic": "...", 
+              "reason": "Why it needs rework"
+            }
+          ],
+          "delete_count": 5
+        }
+        1. KEEP (Direct JSON Output):
+           - Use this for cards that are factually correct, concise, and exam-ready.
+           - Criteria: The answer (back) must be shorter than 3 sentences and formatted clearly (e.g., bullet points).
+        
+        2. DELETE (Ignore):
+           - Organizational data (dates, room numbers, job offers, professor contact info).
+           - Cards where the front is a single generic word (e.g., "Note", "Context") that is not a technical term.
+           - Exact duplicates or redundant information already covered in better cards.
+        
+        3. REWORK :
+           - WHEN: The content is exam-relevant, but the formatting or phrasing is poor.
+           - WHY (Specific Criteria):
+             * 'Wall of Text': The answer is a long, unstructured paragraph.
+             * 'Vague Question': The question is too broad (e.g., "What about Ethics?").
+             * 'Single-Word Issue': The front is an important technical term (e.g., "Cosine Similarity"), but the back is incomplete, messy, or lacks a clear definition.
+            - MANDATORY: For every card in the 'rework' list, you MUST provide a 'reason' field 
+                explaining exactly what is wrong (e.g., 'Wall of Text', 'No Question Mark').
+           - HOW: Add them in the "rework" section.
+        
+        IMPORTANT: 
+        - Every input card must be assigned to exactly ONE category (keep, rework, or delete).
+        - The "rework" section contains the original fields plus the "reason".
+        """
 
         user_prompt = f"""
                 Filter and condense the following list of cards. Ensure the result is manageable and focuses on the most important exam-relevant knowledge.
@@ -52,20 +81,51 @@ class AnkiGen:
                 INPUT_CARDS:
                 {json.dumps(cards)}
                 """
-        return self.run_prompt(system_prompt,user_prompt,"Rework error")
 
+        return self.run_prompt(system_prompt, user_prompt, "Rework error", 2)
 
+    def rework_flashcard(self, flashcard: list[dict]):
+        """Improves flashcards
+        Input: A worse Flashcard to improve
+        Output: Reworked Flashcard"""
+        rework_system_prompt = r"""You are an Anki Refinement Specialist. Your task is to take a list of sub-optimal flashcards and transform them into high-quality, atomic learning units.
 
+        ### OBJECTIVES:
+        - MANDATORY: Every 'front' must be a grammatically correct, self-contained question ending with a question mark; if the input is a statement or a noun, you MUST rephrase it into a 'How', 'What', 'Why', or 'Which' question.
+        - Fix 'Wall of Text' by using clear bullet points (max 3 per card).
+        - Clarify 'Vague Questions' by making the front specific.
+        - Expand 'Single-Word Issues' into clear definitions.
+        - Keep the language professional and concise.
+
+        ### OUTPUT FORMAT:
+        You MUST return a valid JSON object with a single list called 'refined_cards':
+        {
+          "refined_cards": [
+            {"front": "...", "back": "...", "topic": "..."},
+            {"front": "...", "back": "...", "topic": "..."}
+          ]
+        }
+        """
+        rework_user_prompt = f"""
+        Improve the following flashcards. Use the provided 'reason' to guide your refinement for each card.
+
+        INPUT_CARDS_TO_FIX:
+        {json.dumps(flashcard)}
+        """
+        print("Rework Worse Cards")
+        print("___________________________________________________________________________________________________")
+        print(flashcard)
+        return self.run_prompt(rework_system_prompt, rework_user_prompt, "Rework error", 1)
 
     def rework(self, cards: list[dict]):
         """reworks created anki cards deletes unnecessary and bad cards"""
         print("rework started")
-        n=math.ceil(len(cards)/self.rework_size)
-        rework_cards=[]
-        pending_tasks=[]
+        n = math.ceil(len(cards) / self.rework_size)
+        rework_cards = []
+        pending_tasks = []
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.workers) as executor:
-            for i in range(0,n):
-                task=executor.submit(self.rework_part,cards[i*self.rework_size:(i+1)*self.rework_size])
+            for i in range(0, n):
+                task = executor.submit(self.rework_part, cards[i * self.rework_size:(i + 1) * self.rework_size])
                 pending_tasks.append(task)
             for future in concurrent.futures.as_completed(pending_tasks):
                 try:
@@ -73,35 +133,32 @@ class AnkiGen:
                 except Exception as e:
                     print(f"Fehler{e}")
         print(rework_cards)
-        embeddet=embedding.delete_dupes(rework_cards)
+        embeddet = embedding.delete_dupes(rework_cards)
         print_line("_____________________________________")
         print(embeddet)
-        return  embeddet
+        return embeddet
 
-
-
-    def createCards(self,language:str,info_label:ctk.CTkLabel):
+    def createCards(self, language: str, info_label: ctk.CTkLabel):
         """running different threads for multiple tasks, AI api calls, creating cards"""
-        cards=[]
-        all_cards=[]
+        cards = []
+        all_cards = []
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.workers) as executor:
             for i in range(self.handler.pages):
-                page=self.handler.get_pdf_page()
-                page_cards=executor.submit(self._createCard_part,page,language)
+                page = self.handler.get_pdf_page()
+                page_cards = executor.submit(self._createCard_part, page, language)
                 cards.append(page_cards)
             for future in concurrent.futures.as_completed(cards):
                 try:
-                 page_cards = future.result()
-                 all_cards.extend(page_cards)
+                    page_cards = future.result()
+                    all_cards.extend(page_cards)
                 except Exception as e:
-                 print(f"Fehler: {e}")
+                    print(f"Fehler: {e}")
         info_label.configure(text="execute card rework ...")
-        final_cards=self.rework(all_cards)
+        final_cards = self.rework(all_cards)
         return final_cards
 
-
-    def _createCard_part(self,input:str,language:str):
+    def _createCard_part(self, input: str, language: str):
         system_prompt = r"""
         You are a professional Flashcard creator. 
         Analyze the provided text and extract the core concepts into flashcards.
@@ -149,46 +206,59 @@ class AnkiGen:
         
         ---
         """
-        return self.run_prompt(system_prompt,user_prompt,"generation error")
+        return self.run_prompt(system_prompt, user_prompt, "generation error", 3)
 
-    def run_prompt(self,system_prompt:str,user_prompt:str,error_message:str):
-        if self.type==1:
+    def run_prompt(self, system_prompt: str, user_prompt: str, error_message: str, case: int):
+        final_cards = []
+        if self.type == 1:
             client = OpenAI(
                 api_key=os.environ.get('DEEPSEEK_API_KEY'),
                 base_url="https://api.deepseek.com"
             )
 
             try:
-                response = client.chat.completions.create(
-                    model="deepseek-chat",
-                    messages=[
-                        {"role": "system", "content": system_prompt},
-                        {"role": "user", "content": user_prompt},
-                    ],
-                    stream=False,
-                    response_format={'type': 'json_object'}
-                )
-
+                messages = [{"role": "system", "content": system_prompt},
+                            {"role": "user", "content": user_prompt}]
+                params = {
+                    "model": "deepseek-chat",
+                    "messages": messages,
+                    "stream": False,
+                    "response_format": {"type": "json_object"}
+                }
+                response = client.chat.completions.create(**params)
                 data = json.loads(response.choices[0].message.content)
-                return data.get("cards", [])
+                if case == 1:
+                    final_cards.extend(data.get("refined_cards", []))
+                elif case == 3:
+                    final_cards.extend(data.get("cards", []))
+                else:
+                    well_cards = data.get("keep", [])
+                    cards_to_improve = data.get("rework", [])
+                    final_cards.extend(well_cards)
+                    if cards_to_improve:
+                        final_cards.extend(self.rework_flashcard(cards_to_improve))
 
             except Exception as e:
                 print(f"{error_message} {e}")
                 return []
 
-        elif self.type==2:
-             try:
-                response = ollama.chat(model=self.model,format='json',options={"num_ctx":4096}, messages=[{"role": "system", "content": system_prompt},
-                                                                   {"role": "user", "content": user_prompt}])
+        elif self.type == 2:
+            try:
+                response = ollama.chat(model=self.model, format='json', options={"num_ctx": 4096},
+                                       messages=[{"role": "system", "content": system_prompt},
+                                                 {"role": "user", "content": user_prompt}])
                 data = json.loads(response.message.content)
-                refined_cards = data.get("cards", [])
-                return refined_cards
-             except Exception as e:
+                if case == 1:
+                    final_cards.extend(data.get("refined_cards", []))
+                elif case == 3:
+                    final_cards.extend(data.get("cards", []))
+                else:
+                    cards_to_improve = data.get("rework", [])
+                    if cards_to_improve:
+                        final_cards.extend(self.rework_flashcard(cards_to_improve))
+                    final_cards.extend(data.get("keep", []))
+
+            except Exception as e:
                 print(f"{error_message} {e}")
                 return []
-
-
-
-
-
-
+        return final_cards
