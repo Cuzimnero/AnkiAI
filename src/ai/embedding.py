d379e72f450afb6e72b66f23d2b6d3a9ff3a2836
Cuzimnero <bergerarne1@gmx.de>
2026-03-28 17:16:31 +0100

    Improved generation for larger pdf files better embedding

--- src/ai/embedding.py
+++ src/ai/embedding.py
@@ -12,13 +12,25 @@ def gen_vector(cards: list[dict]):
 
 
 def delete_dupes(cards: list[dict]):
+    deleting_value = 0.80
     if cards:
+        if len(cards) > 200:
+            print("____________________________________________")
+            print("TOO MANY CARDS dropping threshold VALUE >200 Cards")
+            print("____________________________________________")
+            deleting_value = 0.6
+        elif len(cards) > 100:
+            print("____________________________________________")
+            print("TOO MANY CARDS dropping threshold VALUE >100 Cards")
+            print("____________________________________________")
+            deleting_value = 0.7
+
         embeddings = gen_vector(cards)
         indices_delete = set()
         sim_Matrix = cosine_similarity(embeddings)
         for i in range(0, sim_Matrix.__len__()):
             for a in range(i + 1, sim_Matrix.__len__()):
-                if sim_Matrix[i][a] >= 0.80:
+                if sim_Matrix[i][a] >= deleting_value:
                     indices_delete.add(a)
         return [card for i, card in enumerate(cards) if i not in indices_delete]
     return cards
